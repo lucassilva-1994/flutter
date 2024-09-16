d5c7aaaad9684fc66b5b55e3a79d33a7e7a9d99a
Lucas Sílva <lucas.silva_1994@outlook.com>
2024-09-15 21:43:37 -0300

    Componentizando os Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/myapp.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(home: HomePage());
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('AppBar Demo')
-      ),
-      body: Center(
-        child: Text('Criando minha primeira aplicação com Flutter'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.zoom_out_sharp),
-        onPressed: () {
-        print('Clicou...');
-      }),
-    );
-  }
 }
